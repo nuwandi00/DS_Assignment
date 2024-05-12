cb43cbb20b37c75c965d6ea1eb0747a86fe06a82
Rishen T.Lithan <100280958+IT21311772@users.noreply.github.com>
2024-05-12 17:27:15 +0530

    Update - Dev Rishen

--- User-Course/routes/authRoutes.js
+++ User-Course/routes/authRoutes.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { getUsers, getEmailByUsername } from "../controllers/authController.js";
+import { getUsers, getEmailByUsername, enrolledModules, coursesInModules } from "../controllers/authController.js";
 
 const router = express.Router();
 
 router.get("/users", getUsers);
 router.get("/getEmail", getEmailByUsername);
+router.post("/enrollmodules", enrolledModules);
+router.get("/modules",coursesInModules);
 
 export default router;
